6a90de8ae042e7b991a8ab8171cccd1981e07ddc
Makay131 <mahmutenesakay9@gmail.com>
2024-07-17 17:28:59 +0300

    init

---
+++ lib/shared/styled_text.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: GoogleFonts.kanit(
+            textStyle: Theme.of(context).textTheme.bodyMedium));
+
+    //before it was
+    /*return Text(text.toUpperCase(),
+        style: Theme.of(context).textTheme.headlineMedium);
+  }*/
+  }
+}
+
+class StyledHeading extends StatelessWidget {
+  const StyledHeading(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text.toUpperCase(),
+        style: GoogleFonts.kanit(
+            textStyle: Theme.of(context).textTheme.headlineMedium));
+  }
+}
+
+class StyledTitle extends StatelessWidget {
+  const StyledTitle(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text.toUpperCase(),
+        style: GoogleFonts.kanit(
+            textStyle: Theme.of(context).textTheme.titleMedium));
+  }
+}
